96838136229ff650d67d14cbf4c78b583a2be499
voler <luhuitoux@gmail.com>
2026-01-19 17:01:05 +0800

    feat: phonebook database 3.15-3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -12,9 +12,9 @@ morgan.token('body', (req, res) => {
   return ''
 })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -49,21 +49,34 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
   const date = new Date()
-  response.send(
+  Person
+    .find({})
+    .then(persons => {
+      response.send(
   `<p>Phonebook has info for ${persons.length} people</p>
   <p>${date}</p>`)
+    })
+  
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(p => {
-    response.json(p)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(p => {
+      if (p) {
+        response.json(p)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(item => item.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response) => {
@@ -94,6 +107,37 @@ app.post('/api/persons/', (request, response) => {
   
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(p => {
+      if (!p) {
+        return response.status(404).end()
+      }
+
+      p.name = name
+      p.number = number
+
+      return p.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, '0.0.0.0', () => {
